3d568e76c1fc74a756bc849e70a249be84b0a2d9
soumohanty <soumohanty@soumohantys-MBP.domain.name>
2022-11-02 21:20:40 +0530

    initialized create-react-app and added text

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learning about Git and deployments!
+        </a>
+        <small>with 30 folks!</small>
+      </header>
+    </div>
+  );
+}
+
+export default App;
